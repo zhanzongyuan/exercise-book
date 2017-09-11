d878be58bac65af3d75ebaeecaa537614aa3c4fe
zongyuanZhan <1450303552@qq.com>
2017-09-11 08:20:07 +0800

    solve 015 in low efficient algorithm

--- 015_3Sum.cpp
+++ 015_3Sum.cpp
@@ -13,52 +13,52 @@
 //  Copyright © 2017年 applecz. All rights reserved.
 //
 
+// 1.0 v
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
-        int temp[3];
-        for (int i=0; i<nums.size();i++){
-            map<int, int> hash_map;
-            map<int, int>::iterator iter;
-            
-            int target=-nums[i];
-            for (int j=0; j<i; j++){
-                int com=target-nums[j];
-                iter=hash_map.find(com);
-                if (iter!=hash_map.end() &&iter->second!=j){
-                    temp[0]=nums[j];
-                    temp[1]=iter->first;
-                    temp[2]=nums[i];
-                    //sort temp[3]
-                    for (int j=0; j<2; j++){
-                        for (int k=j+1; k<3; k++)
-                            if (temp[k]<temp[j]){
-                                int p=temp[k];
-                                temp[k]=temp[j];
-                                temp[j]=p;
-                            }
-                    }
-                    if (result.size()==0) result.push_back(vector<int>{temp[0], temp[1], temp[2]});
-                    else
-                        for (vector<vector<int> >::iterator j=result.begin(); j!=result.end(); j++){
-                            //apart from the same
-                            if (temp[0]==j->at(0)&&temp[1]==j->at(1)&&temp[2]==j->at(2)) break;
-                            //insert in the order
-                            if ((temp[0]<j->at(0))
-                                ||(temp[0]==j->at(0)&&temp[1]<j->at(1))
-                                ||(temp[0]==j->at(0)&&temp[1]==j->at(1)&&temp[2]<j->at(2))){
-                                result.insert(j, vector<int>{temp[0], temp[1], temp[2]});
-                                break;
-                            }
-                            if (j+1==result.end())
-                                j=result.insert(j+1, vector<int>{temp[0], temp[1], temp[2]});
-                            
-                        }
-                }
-                hash_map.insert(pair<int, int>(nums[j], j));
-            }
+        
+        //排序
+        int n=nums.size();
+        priority_queue<int> q;
+        int *sortedNums=new int[nums.size()];
+        for(int i=0; i<n; i++){
+            q.push(nums[i]);
         }
+        for (int i=n-1; i>=0; i--){
+            sortedNums[i]=q.top();
+            q.pop();
+        }
+        int i=0;
+        while(i<n){
+            int target=-sortedNums[i];
+            map<int, int> indexMap;
+            int j;
+            for (j=i+1; j<n; j++) indexMap[sortedNums[j]]=j;
+            j=i+1;
+            while(j<n){
+                int com=target-sortedNums[j];
+                map<int, int>::iterator iter=indexMap.find(com);
+                if (iter!=indexMap.end()){
+                    if (iter->second<=j) break;
+                    else{
+                        result.push_back(vector<int>{sortedNums[i], sortedNums[j], com});
+                        indexMap.erase(com);
+                    }
+                }
+                j++;
+                while(j<n&&j+1<n&&sortedNums[j-1]==sortedNums[j]&&sortedNums[j+1]==sortedNums[j]) j++;
+            }
+            
+            while(i+1<n&&sortedNums[i]==sortedNums[i+1]) i++;
+            i++;
+        }
+        
+        delete []sortedNums;
+        int temp[3];
+        //排序
+        
         return result;
     }
 };
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # leetcode
 The repository is used to store my code for problems in leetcode
+
+## 015 three sum
+2017-09-11 08:18
+算法accpted！但是运行时间特别慢，只击败1%的cpp代码
+算法学习改进中......
